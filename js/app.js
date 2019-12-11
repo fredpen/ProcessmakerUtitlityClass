12de29480f7753a507f35d5f1fc860ac3a5a7795
fredpen <fredrickksola@gmail.com>
2019-12-11 17:23:20 +0100

    fix js bug

--- js/app.js
+++ js/app.js
@@ -5,6 +5,6 @@ var currentTabId = 'home';
 function switchTab(tabid)
 {
     $("#" + tabid).removeClass('dontshow');//fade in the selected tab
-    $("#" + currentTabId).addClass('dontshow');//fade in the selected tab
+    $("#" + currentTabId).addClass('dontshow');//fade out the current tab
     return currentTabId = tabid; // update the currentabID
 }
